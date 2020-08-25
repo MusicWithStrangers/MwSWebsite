8b5e691d782ddb85d3cc1a1985227db1ec6ee7d7
Xano Trevisan Kothe <xanokothe@gmail.com>
2020-08-26 01:55:01 +0200

    Fix User::userPayedNow()
    Signed-off-by: Xano Trevisan Kothe <xanokothe@gmail.com>

--- adm_program/system/classes/User.php
+++ adm_program/system/classes/User.php
@@ -1399,7 +1399,7 @@ class User extends TableAccess
     public function userPayedNow()
     {
         $now = new DateTime();
-        $sql_payednow='SELECT pay_id,pay_user from mws__payments inner join mws__contribution_fees on mws__contribution_fees.fee_id = mws__payments.pay_contribution_id where pay_user = '.$this->getValue('usr_id'). ' and mws__contribution_fees.fee_from<CURRENT_TIMESTAMP and mws__contribution_fees.fee_to>CURRENT_TIMESTAMP' ;
+        $sql_payednow='SELECT pay_id,pay_user from mws__payments inner join mws__contribution_fees on mws__contribution_fees.fee_id = mws__payments.pay_contribution_id where mws__payments.pay_status = 1 and pay_user = '.$this->getValue('usr_id'). ' and mws__contribution_fees.fee_from<CURRENT_TIMESTAMP and mws__contribution_fees.fee_to>CURRENT_TIMESTAMP' ;
         $pdoStatement = $this->db->queryPrepared($sql_payednow);
         $payed=$pdoStatement->rowCount();
         if ($payed>0)
